0635387e00f1ccdbf46473f044b03b81d2d80298
Gabriel Gimnang <ggimna499@west-mec.org>
2025-05-16 15:48:14 -0700

    test commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: "./", //important for correct paths yeah
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
